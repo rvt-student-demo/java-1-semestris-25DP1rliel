9ac1ec29cbebfea46c136936e6707370b185d510
25DP1rliel <A240593RL@rvt.lv>
2025-09-15 12:17:15 +0300

    print statement

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,8 @@
-package rvt;
+package rvt; //package
 
 public class App {
     public static void main(String[] args) {
         // Your code goes here...
+        System.out.println("Gadās");
     }
 }
