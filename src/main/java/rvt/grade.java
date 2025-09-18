7af41952ddda616bd0e747c49b3ba345e81bddc6
25DP1rliel <A240593RL@rvt.lv>
2025-09-18 12:31:37 +0300

    Grades and Points

---
+++ src/main/java/rvt/grade.java
@@ -0,0 +1,36 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class grade {
+        public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Score:");
+        int rez = Integer.parseInt(scan.nextLine());
+        System.out.println("");
+        if(rez > 100) {
+            System.out.println("Increaible!");
+        }
+        else if(rez > 89) {
+            System.out.println("5");
+        }
+        else if(rez > 79) {
+            System.out.println("4");
+        }
+        else if(rez > 69) {
+            System.out.println("3");
+        }
+        else if(rez > 59) {
+            System.out.println("2");
+        }
+        else if(rez > 49) {
+            System.out.println("1");
+        }
+        else if(rez > 0) {
+            System.out.println("Failed");
+        }
+        else if(rez < 0) {
+            System.out.println("Impossible!");
+        }
+    }
+}
