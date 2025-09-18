00a36866b8d9749db0774130c7162a02d476fb4e
25DP1rliel <A240593RL@rvt.lv>
2025-09-18 12:37:09 +0300

    odd or even

---
+++ src/main/java/rvt/eoo.java
@@ -0,0 +1,17 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class eoo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = Integer.parseInt(scan.nextLine());
+        if(n%2==0) {
+            System.out.println("The number " + n + " is even");
+        }
+        else {
+            System.out.println("The number " + n + " is odd");
+        }
+    }
+}
