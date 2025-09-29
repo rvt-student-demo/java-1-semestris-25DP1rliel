7e5668030fd548eee1ce73a456e04962571d75d7
25DP1rliel <A240593RL@rvt.lv>
2025-09-29 12:37:51 +0300

    Division With 3

---
+++ src/main/java/rvt/div3.java
@@ -0,0 +1,23 @@
+package rvt;
+import java.util.Scanner;
+
+public class div3 {
+    
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+        System.out.println("Ievadiet sākuma vērtību: ");
+        int start = Integer.parseInt(scan.nextLine());
+        System.out.println("Ievadiet beigu vērtību: ");
+        int end = Integer.parseInt(scan.nextLine());
+        scan.close();
+        System.out.println("Skaitļi kas dalās ar 3: ");
+        div3(start,end);
+    }
+        private static void div3(int start,int end) {
+            for(int i=start;i<=end;i++) {
+                if(i%3==0) {
+                    System.out.println(i);
+                }
+            }
+    }
+}
